b570aadf9e4a0ed2d5d26a3662afee23a036cd51
Masoumeh Hashemi <masoomeh.hashemi92@gmail.com>
2020-05-28 14:35:55 +0430

    typegraphql-resolver

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { ApolloServer} from "apollo-server-express";
+import * as Express from "express";
+import { buildSchema} from "type-graphql";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { RegisterResolver } from "./modules/user/Register";
+
+// @Resolver()
+// class HelloResolver {
+//   @Query(() => String)
+//   async hello() {
+//     return "Hello Word!"
+//   }
+// }
+
+
+// @Resolver()
+// class ByeResolver {
+//   @Query(() => String)
+//   async bye() {
+//     return "Bye Word!"
+//   }
+// }
+
+const main = async ()=>{
+
+    await createConnection();
+    const schema = await buildSchema({
+        // resolvers: [HelloResolver,ByeResolver],
+        resolvers:[RegisterResolver]
+      });
+    const apolloServer = new ApolloServer({schema});
+    const app = Express();
+    apolloServer.applyMiddleware({app})
+    app.listen(4000,()=> {
+        console.log('Hi i"m listening');
+    })
+};
+
+main();
