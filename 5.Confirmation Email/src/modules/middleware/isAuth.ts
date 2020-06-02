b9f210b4b0b2b56b61c2a4abbf5ed7511bf4af8e
Masoumeh Hashemi <masoomeh.hashemi92@gmail.com>
2020-06-02 14:02:56 +0430

    add confirm user check to loginresolver

---
+++ 5.Confirmation Email/src/modules/middleware/isAuth.ts
@@ -0,0 +1,12 @@
+import { MiddlewareFn } from "type-graphql/dist/interfaces/Middleware";
+import { MyContext } from "src/types/MyContext";
+
+
+export const isAuth: MiddlewareFn<MyContext> = async ({context} , next) =>
+{
+    if (!context.req.session!.userId){
+        throw new Error("you are not authenticated");
+    }
+
+    return next();
+}
---
+++ 5.Confirmation Email/src/modules/user/Register.ts
@@ -0,0 +1,41 @@
+import { Resolver, Query, Mutation, Arg, FieldResolver, Root, /*Authorized,*/ UseMiddleware} from "type-graphql";
+import {User} from "../../entity/User"
+import bcrypt from 'bcryptjs'; 
+import { RegisterInput } from "./register/RegisterInput";
+import { isAuth } from "../middleware/isAuth";
+@Resolver(User)
+export class RegisterResolver {
+  // @Authorized()
+  @UseMiddleware(isAuth)
+  @Query(() => String)
+  async hello() {
+    return "Hello you are logged in"
+  }
+
+  @FieldResolver()
+  async name(@Root() parent:User){
+    return `${parent.firstName} ${parent.lastName}`;
+  }
+  @Mutation(() => User)
+  async register(@Arg("data"){
+    email,
+    firstName,
+    lastName,
+    password
+  }:RegisterInput,
+
+  ):Promise<User>
+  {
+    const hashedPassword = await bcrypt.hash(password,12)  
+    const user = await User.create(
+    {
+      firstName,
+      lastName,
+      email,
+      password:hashedPassword
+    }).save();
+
+    return user;
+
+  }
+}
